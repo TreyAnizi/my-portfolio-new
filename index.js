2bfd283540a2b2b2e636b4b3b976105a8f505db9
TreyAnizi <tracyaniziamoro1@gmail.com>
2025-03-23 12:16:13 +0000

    javaScript added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.getElementById ("click").innertext = "I am capable!"
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+const text = "jhgyjcghctxkuvx ygxgvtjbcyy ygync  ygynn gvn gv  tccn t gcvt cvt vfdsahyun gdbctfd cgtfhgdvtfybdshcfy gdhcthdcgthdyj dhcbgcdhydgyds hghubcyghgdhyyduh dgybhvchbychdg";
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById('typewriter').textContent += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 20); // Adjust speed here
+    }
+}
+
+typeWriter();
