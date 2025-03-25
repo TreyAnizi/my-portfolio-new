07eadb43244a584e36373fae3d6d2e24de7e0c28
TreyAnizi <tracyaniziamoro1@gmail.com>
2025-03-25 17:10:17 +0000

    tailwind

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-const text = "jhgyjcghctxkuvx ygxgvtjbcyy ygync  ygynn gvn gv  tccn t gcvt cvt vfdsahyun gdbctfd cgtfhgdvtfybdshcfy gdhcthdcgthdyj dhcbgcdhydgyds hghubcyghgdhyyduh dgybhvchbychdg";
+const text = "jhgyjcghctxkuvx ygxgvtjbcyy ygync  ygynn gvn gv  tccn t gcvt cvt vfdsahyun gdbctfd cgtfhgdvtfybdshcfy gdhcthdcgthdyj dhcbgcdhydgyds hghubcyghgdhyyduh dgybhvchbychdg bsfbbyfncxmbhgfhd jgdygicmn gfygbd hgdbjbyifvd hgdnbtfxcjf hvu hghgv hgvfufm g mbuhtfng7gf ng7yfdjjfg7nfb7tfd uguig ";
 let index = 0;
 
 function typeWriter() {
     if (index < text.length) {
         document.getElementById('typewriter').textContent += text.charAt(index);
         index++;
-        setTimeout(typeWriter, 20); // Adjust speed here
+        setTimeout(typeWriter, 10); // Adjust speed here
     }
 }
 
